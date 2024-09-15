6fdb94371dbecac9bbcdf41d27ad0436234fcc95
Vatsa <vatsanatural@outlook.com>
2024-09-15 22:49:38 +0530

    Completed adding API for Article and Article In particular category

--- blogRestAPI/serializers.py
+++ blogRestAPI/serializers.py
@@ -1,6 +1,12 @@
 from rest_framework import serializers
-from blogapp.models import CategoryClass
+from blogapp.models import CategoryClass, Article
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model=CategoryClass
+        fields='__all__'
+
+
+class ArticleSerializer(serializers.ModelSerializer):
+    class Meta:
+        model=Article
         fields='__all__'
--- blogRestAPI/views.py
+++ blogRestAPI/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
-from .serializers import CategorySerializer
-from blogapp.models import CategoryClass
+from .serializers import CategorySerializer, ArticleSerializer
+from blogapp.models import CategoryClass, Article
 from rest_framework import generics
 # Create your views here.
 
-class CategoryListView(generics.ListAPIView):
+class CategoryListView(generics.ListCreateAPIView):
     queryset= CategoryClass.objects.all()
     serializer_class = CategorySerializer
+
+class CategoryDetailsView(generics.RetrieveUpdateDestroyAPIView):
+    queryset= CategoryClass.objects.all()
+    serializer_class = CategorySerializer
+
+class ArticleListView(generics.ListCreateAPIView):
+    queryset= Article.objects.all()
+    serializer_class = ArticleSerializer
+
+class ArticleListView(generics.RetrieveUpdateDestroyAPIView):
+    queryset= Article.objects.all()
+    serializer_class = ArticleSerializer
+
+class ArticlesInCategoryListView(generics.ListAPIView):
+    serializer_class = ArticleSerializer
+    def get_queryset(self):
+        category = self.kwargs['category']
+        return Article.objects.filter(category = CategoryClass.objects.get(category = category).pk)
