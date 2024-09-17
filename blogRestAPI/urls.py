4eeb33f3019dc6c17d60c9c661f05f63ba3c84e8
Vatsa <vatsanatural@outlook.com>
2024-09-17 16:12:58 +0530

    Changed Article In Category to Raise 404 For non existing categy request

--- blogRestAPI/urls.py
+++ blogRestAPI/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('categorylist/', views.CategoryListView.as_view()),
     path('categorylist/<int:pk>', views.CategoryDetailsView.as_view()),
     path('articlelist/', views.ArticleListView.as_view()),
-    path('articlelist/<int:pk>', views.ArticleListView.as_view()),
+    path('articlelist/<int:pk>', views.ArticleDetailsView.as_view()),
     path('articleincategorylist/<str:category>', views.ArticlesInCategoryListView.as_view()),
 ] 
--- blogRestAPI/views.py
+++ blogRestAPI/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from .serializers import CategorySerializer, ArticleSerializer
 from blogapp.models import CategoryClass, Article
+from django.shortcuts import get_object_or_404
+from django.http import Http404
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 # Create your views here.
 
 class CategoryListView(generics.ListCreateAPIView):
@@ -15,8 +18,9 @@ class CategoryDetailsView(generics.RetrieveUpdateDestroyAPIView):
 class ArticleListView(generics.ListCreateAPIView):
     queryset= Article.objects.all()
     serializer_class = ArticleSerializer
+    permission_classes = [IsAuthenticated]
 
-class ArticleListView(generics.RetrieveUpdateDestroyAPIView):
+class ArticleDetailsView(generics.RetrieveUpdateDestroyAPIView):
     queryset= Article.objects.all()
     serializer_class = ArticleSerializer
 
@@ -24,4 +28,7 @@ class ArticlesInCategoryListView(generics.ListAPIView):
     serializer_class = ArticleSerializer
     def get_queryset(self):
         category = self.kwargs['category']
-        return Article.objects.filter(category = CategoryClass.objects.get(category = category).pk)
+        try:
+            return Article.objects.filter(category = CategoryClass.objects.get(category = category).pk)
+        except CategoryClass.DoesNotExist:
+            raise Http404("No such category exist.")
